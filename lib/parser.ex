e274aa9978b5e6b7b22444249ff3790a491705ad
Jakub Gonet <jakub.gonet0@gmail.com>
2017-12-22 08:13:41 +0100

    Simplify parser

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,13 +1,16 @@
 defmodule Parser do
   @spec parse(binary) :: list
   def parse(str) do
-    {:ok, tokens, _} = str |> to_charlist() |> :assembler_lexer.string()
-    {:ok, list} = :assembler_parser.parse(tokens)
+    {:ok, list} = str
+                  |> tokenize()
+                  |> :assembler_parser.parse()
     list
   end
 
   def tokenize(str) do
-    {:ok, tokens, _} = str |> to_charlist() |> :assembler_lexer.string()
+    {:ok, tokens, _} = str
+                        |> to_charlist()
+                        |> :assembler_lexer.string()
     tokens
   end
 end
