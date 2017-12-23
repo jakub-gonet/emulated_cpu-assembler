6f3c5c7a10db6f7f11b0a5a4d0f0472785add41a
Jakub Gonet <jakub.gonet0@gmail.com>
2017-12-23 14:42:20 +0100

    Add EBN form to make things clear

--- lib/assembler.ex
+++ lib/assembler.ex
@@ -1,16 +1,48 @@
 defmodule Assembler do
   @moduledoc """
-  Documentation for Assembler.
-  """
+    Assembler is used to assembly .ecpu files into compiled form for
+    Emulated CPU.
 
-  @doc """
-  Hello world.
+    EBN form for this language is like following:
 
-  ## Examples
+    letter = "A" | "B" | "C" | "D" | "E" | "F" | "G"
+       | "H" | "I" | "J" | "K" | "L" | "M" | "N"
+       | "O" | "P" | "Q" | "R" | "S" | "T" | "U"
+       | "V" | "W" | "X" | "Y" | "Z" | "a" | "b"
+       | "c" | "d" | "e" | "f" | "g" | "h" | "i"
+       | "j" | "k" | "l" | "m" | "n" | "o" | "p"
+       | "q" | "r" | "s" | "t" | "u" | "v" | "w"
+       | "x" | "y" | "z" | "0" | "1" | "2" | "3"
+       | "4" | "5" | "6" | "7" | "8" | "9" | "@"
+       | "#" | "!" | "_";
 
-      iex> Assembler.hello
-      :world
+    digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" ;
+    whitespace = "\ " | "\t" | "\n";
 
+    program = {assigment},{command},"HLT";
+
+    command = opcode, whitespace,
+            [(register | address | address in register),[ ",",
+            (register | address | address in register | const)]]
+    assigment = identifier, "<=", number, whitespace;
+    identifier = letter, {letter | digit};
+    label = identifier, ":", whitespace;
+    number = ["-"], digit, {digit};
+
+
+    where:
+    definition	=
+    concatenation	,
+    termination	;
+    alternation	|
+    optional	[ ... ]
+    repetition	{ ... }
+    grouping	( ... )
+    terminal string	" ... "
+    terminal string	' ... '
+    comment	(* ... *)
+    special sequence	? ... ?
+    exception	-
   """
   def hello do
     :world
