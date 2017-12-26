8858db1a388e4755c100681322ffff3e123eb071
Jakub Gonet <jakub.gonet0@gmail.com>
2017-12-26 23:19:35 +0100

    Add lookup tables for opcodes and addressing modes

---
+++ lib/lookup_tables/addressing_modes.ex
@@ -0,0 +1,23 @@
+defmodule AddressingModesLookupTable do
+@moduledoc """
+Contains lookup table for addressing modes
+"""
+
+@addressing_modes %{
+  CONST: 0,
+  REGISTER: 1,
+  ADDRESS: 2,
+  ADDRESS_IN_REGISTER: 3
+}
+
+
+@doc """
+Gets addressing mode number
+
+Returns addressing mode number or :error
+"""
+def get_addressing_mode_number(addressing_mode) do
+  Map.fetch!(@addressing_modes, addressing_mode)
+end
+
+end
---
+++ lib/lookup_tables/opcodes.ex
@@ -0,0 +1,58 @@
+defmodule OpcodesLookupTable do
+@moduledoc """
+Contains lookup table for opcodes and functions getting values from them
+"""
+
+@opcodes %{
+  NOP:    %{number: 0,  required_args: 0},
+  HLT:    %{number: 1,  required_args: 0},
+  MOV:    %{number: 2,  required_args: 2},
+  JMP:    %{number: 3,  required_args: 1},
+  JE:     %{number: 4,  required_args: 1},
+  JNE:    %{number: 5,  required_args: 1},
+  JL:     %{number: 6,  required_args: 1},
+  JLE:    %{number: 7,  required_args: 1},
+  JG:     %{number: 8,  required_args: 1},
+  JGE:    %{number: 9,  required_args: 1},
+  INC:    %{number: 10, required_args: 1},
+  DEC:    %{number: 11, required_args: 1},
+  ADD:    %{number: 12, required_args: 2},
+  SUB:    %{number: 13, required_args: 2},
+  MUL:    %{number: 14, required_args: 2},
+  DIV:    %{number: 15, required_args: 2},
+  AND:    %{number: 16, required_args: 2},
+  OR:     %{number: 17, required_args: 2},
+  XOR:    %{number: 18, required_args: 2},
+  NOT:    %{number: 19, required_args: 1},
+  RSHFT:  %{number: 20, required_args: 2},
+  LSHFT:  %{number: 21, required_args: 2},
+  CMP:    %{number: 22, required_args: 2}
+}
+
+@doc """
+Gets opcode details from lookup table.
+
+Returns tuple: {:ok, <opcode tuple>} or :error}
+"""
+def get_opcode(opcode) do
+   Map.fetch(@opcodes, opcode)
+end
+
+@doc """
+Gets required arguments number from given opcode
+
+Returns required arguments number
+"""
+def get_required_arguments_number({:ok, opcode_tuple}), do:
+  opcode_tuple.required_args
+
+@doc """
+Gets opcode number from given opcode
+
+Returns opcode number or
+"""
+def get_opcode_number({:ok, opcode_tuple}) do
+  opcode_tuple.number
+end
+
+end
