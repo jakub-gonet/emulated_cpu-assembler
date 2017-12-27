f2a8124dacab688d77b85ade7eb55ecae2a99626
Jakub Gonet <jakub.gonet0@gmail.com>
2017-12-27 21:51:23 +0100

    Change arguments def from default to private functions

--- lib/assembler/arguments.ex
+++ lib/assembler/arguments.ex
@@ -1,19 +1,17 @@
 defmodule Assembler.Arguments do
 
-  def get_args_values(args, values_list \\ [])
-  def get_args_values([], values_list) do
-    {:ok, values_list}
-  end
-  def get_args_values([arg], _values_list) when is_atom(arg), do:
-    arg
-  def get_args_values([[:CONST | [value]] | _second], [])
-  when not is_atom(value) do
+  def get_args_values(args), do:
+    _get_args_values(args, [])
+  defp _get_args_values([], values_list), do: {:ok, values_list}
+  defp _get_args_values([label], _values_list) when is_atom(label), do: label
+  defp _get_args_values([[:CONST | [value]] | _second], [])
+  when not is_atom(value), do:
     {:error, :first_arg_const}
-  end
-  def get_args_values([first | second], values_list) do
+  defp _get_args_values([first | second], values_list) when is_list(first) do
         [_addr_mode | [value | _]] = first
         values_list = values_list ++ [value]
-        get_args_values(second, values_list)
+        _get_args_values(second, values_list)
   end
+  defp _get_args_values(_, _), do: {:error, :wrong_arg_type}
 
 end
