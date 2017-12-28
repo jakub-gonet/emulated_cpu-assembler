cf9b3af74f144995051f0fa24fc189710dd5ecf4
Jakub Gonet <jakub.gonet0@gmail.com>
2017-12-28 19:31:51 +0100

    Add escript main module in mix.exs

--- mix.exs
+++ mix.exs
@@ -7,8 +7,13 @@ defmodule Assembler.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript()
     ]
+  end
+
+  def escript do
+    [main_module: Assembler.CLI]
   end
 
   # Run "mix help compile.app" to learn about applications.
