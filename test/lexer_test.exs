2b0edad0c8cbcc7fb92affa2253b64fe7eb5bc8e
Jakub Gonet <jakub.gonet0@gmail.com>
2017-12-28 20:01:07 +0100

    Update parser and lexer tests

--- test/lexer_test.exs
+++ test/lexer_test.exs
@@ -3,17 +3,17 @@ defmodule AssemblerLexerTest do
   import Parser, only: [tokenize: 1]
 
 test "tokenize/1 - valid syntax is passed" do
-  assert tokenize("MOV &[1], 2") ==  [{:atom, 1, :MOV},
+  assert tokenize("MOV &[1], 2") ==  {:ok, [{:atom, 1, :MOV},
            {:&, 1}, {:"[", 1}, {:int, 1, 1}, {:"]", 1},
-           {:",", 1}, {:int, 1, 2}]
+           {:",", 1}, {:int, 1, 2}], 1}
 end
 
 test "tokenize/1 - valid syntax is passed, but lowercase" do #should treat it as identifier
-  assert tokenize("mov 2, 2") == [{:atom, 1, :mov}, {:int, 1, 2}, {:",", 1}, {:int, 1, 2}]
+  assert tokenize("mov 2, 2") == {:ok, [{:atom, 1, :mov}, {:int, 1, 2}, {:",", 1}, {:int, 1, 2}], 1}
 end
 
 test "tokenize/1 - valid 1 arg opcode is used" do
-  assert tokenize("JMP loop") == [{:atom, 1, :JMP}, {:atom, 1, :loop}]
+  assert tokenize("JMP loop") == {:ok, [{:atom, 1, :JMP}, {:atom, 1, :loop}], 1}
 end
 
 end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -3,40 +3,56 @@ defmodule AssemblerParserTest do
   import Parser, only: [parse: 1]
 
 test "parse/1" do
-  expected = [%{label: :loop},
+  tokenized = [{:atom, 1, :loop}, {:":", 1}, {:atom, 2, :MOV}, {:"[", 2}, {:int, 2, 2},
+  {:"]", 2}, {:",", 2}, {:"[", 2}, {:int, 2, 1}, {:"]", 2}, {:atom, 3, :JMP},
+  {:atom, 3, :loop}]
+
+  expected = {:ok, [%{label: :loop},
               %{operation: [:MOV, [:REGISTER, 2], [:REGISTER, 1]]},
-              %{operation: [:JMP, [:CONST, :loop]]}]
-  assert parse("loop:\nMOV [2], [1]\nJMP loop\n") == expected
+              %{operation: [:JMP, [:CONST, :loop]]}]}
+
+
+  assert parse(tokenized) == expected
 end
 test "parse/1 - parenthesis doesn't match" do
-  assert_raise(MatchError, fn -> parse("MOV [13, 8") end)
-  assert_raise(MatchError, fn -> parse("MOV 13], 8") end)
+  tokenized = [{:atom, 1, :MOV}, {:"[", 1}, {:int, 1, 13}, {:",", 1}, {:int, 1, 8}]
+  assert {:error, _} = parse(tokenized)
+
+  tokenized = [{:atom, 1, :MOV}, {:int, 1, 13}, {:"[", 1},  {:",", 1}, {:int, 1, 8}]
+  assert {:error, _} = parse(tokenized)
 end
 
 test "parse/1 - too much arguments" do
-  assert_raise(MatchError, fn -> parse("MOV [13], 8, 8") end)
-  assert_raise(MatchError, fn -> parse("MOV [13], 8 8") end)
-end
+  tokenized = [{:atom, 1, :MOV}, {:"[", 1}, {:int, 1, 13}, {:"]", 1}, {:",", 1},
+  {:int, 1, 8}, {:",", 1}, {:int, 1, 8}]
+  assert {:error, _} = parse(tokenized)
 
-test "parse/1 - added \\n inside operation" do
-  assert_raise(MatchError, fn -> parse("MOV [1\n3], 8") end)
-  assert_raise(MatchError, fn -> parse("MOV [1\n3], 8") end)
-  assert parse("MOV [13],\n 8") == [%{operation: [:MOV, [:REGISTER, 13], [:CONST, 8]]}]
+  tokenized = [{:atom, 1, :MOV}, {:"[", 1}, {:int, 1, 13}, {:"]", 1}, {:",", 1},
+  {:int, 1, 8}, {:int, 1, 8}]
+  assert {:error, _} = parse(tokenized)
 end
 
 test "parse/1 - comma is missing" do
-  assert_raise(MatchError, fn -> parse("MOV [13] 8") end)
+  tokenized = [{:atom, 1, :MOV}, {:"[", 1}, {:int, 1, 13}, {:"]", 1}, {:int, 1, 8}]
+  assert {:error, _} = parse(tokenized)
 end
 
 test "parse/1 - number / label isn't used as value" do
-  assert_raise(MatchError, fn -> parse("MOV [k], 8") end)
-  assert_raise(MatchError, fn -> parse("MOV test, 8") end)
-  assert_raise(MatchError, fn -> parse("MOV [:hello], 8") end)
+  tokenized = [{:atom, 1, :MOV}, {:"[", 1}, {:atom, 1, :k}, {:"]", 1}, {:int, 1, 8}]
+  assert {:error, _} = parse(tokenized)
+
+  tokenized = [{:atom, 1, :MOV}, {:atom, 1, :test}, {:int, 1, 8}]
+  assert {:error, _} = parse(tokenized)
+
+  tokenized = [{:atom, 1, :MOV}, {:"[", 1}, {:":", 1}, {:atom, 1, :hello}, {:"]", 1},
+  {:int, 1, 8}]
+  assert {:error, _} = parse(tokenized)
 end
 
 test "wrong symbol is used" do
-  assert_raise(MatchError, fn -> parse("MOV $[2], 8") end)
-  assert_raise(MatchError, fn -> parse("MOV (2), 8") end)
+  tokenized =  [{:atom, 1, :MOV}, {:atom, 1, :"$"}, {:"[", 1}, {:int, 1, 2}, {:"]", 1},
+  {:",", 1}, {:int, 1, 8}]
+  assert {:error, _} = parse(tokenized)
 end
 
 end
